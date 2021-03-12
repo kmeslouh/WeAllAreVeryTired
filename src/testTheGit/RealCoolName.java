5a23a0a7d521ee61dccc1f318f98be1804e7542f
kmeslouh <k.meslouh06@gmail.com>
2021-03-11 21:12:52 -0500

    Cool

---
+++ src/testTheGit/RealCoolName.java
@@ -0,0 +1,14 @@
+package testTheGit;
+
+public class RealCoolName {
+
+	public static void main(String[] args) {
+
+		some code i am writing
+		standing in the dark
+		singing my song
+		holding my pen
+		making it all go away
+	}
+
+}
